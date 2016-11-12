37d9e1efd2731506a0b4ee90898b3ad824999183
Sayan Paul <sayan.paul.us@gmail.com>
2016-11-12 01:54:22 -0800

    Add recyclerview

---
+++ app/src/main/java/com/mdb/statwiz/FunctionViewAdapter.java
@@ -0,0 +1,65 @@
+package com.mdb.statwiz;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sayan Paul on 11/12/2016.
+ */
+
+public class FunctionViewAdapter extends RecyclerView.Adapter<FunctionViewAdapter.FunctionViewHolder> {
+
+    Context context;
+    ArrayList<Function> functionsList;
+    ColorGenerator generator = ColorGenerator.MATERIAL;
+
+    public FunctionViewAdapter(Context context, ArrayList<Function> functions) {
+        this.context = context;
+        this.functionsList = functions;
+    }
+
+    @Override
+    public void onBindViewHolder(FunctionViewHolder holder, int position) {
+        Function f = functionsList.get(position);
+        String letter = String.valueOf(f.name.charAt(0));
+
+        TextDrawable drawable = TextDrawable.builder()
+                .buildRound(letter, generator.getRandomColor());
+
+        holder.letterView.setImageDrawable(drawable);
+        holder.itemName.setText(f.name);
+    }
+
+    @Override
+    public FunctionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.function_row_view, null, false);
+        return new FunctionViewHolder(v);
+    }
+
+    @Override
+    public int getItemCount() {
+        return functionsList.size();
+    }
+
+    class FunctionViewHolder extends RecyclerView.ViewHolder {
+        public ImageView letterView;
+        public TextView itemName;
+
+        public FunctionViewHolder(View v) {
+            super(v);
+
+            letterView = (ImageView) v.findViewById(R.id.function_item_letter);
+            itemName = (TextView) v.findViewById(R.id.function_item_name);
+        }
+    }
+}
