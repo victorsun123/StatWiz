c32f24eb5ad38d5111f9415c39899ac14c103172
victorsun123 <victor7sun@gmail.com>
2016-11-12 19:17:15 -0800

    drawables

--- app/src/main/java/com/mdb/statwiz/DescriptiveOutputActivity.java
+++ app/src/main/java/com/mdb/statwiz/DescriptiveOutputActivity.java
@@ -1,13 +1,25 @@
 package com.mdb.statwiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class DescriptiveOutputActivity extends AppCompatActivity {
+public class DescriptiveOutputActivity extends Fragment {
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.activity_descriptive_input, container, false);
+
+        return layout;
+    }
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_descriptive_output);
     }
 }
