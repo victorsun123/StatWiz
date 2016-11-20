955a6272ca93bb7d52971bd79466c94f63d1bf7e
victorsun123 <victor7sun@gmail.com>
2016-11-20 01:51:43 -0800

    Fix some backstack issues

--- app/src/main/java/com/mdb/statwiz/MainActivity.java
+++ app/src/main/java/com/mdb/statwiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.mdb.statwiz;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -25,7 +26,7 @@ public class MainActivity extends AppCompatActivity
 
         MainContentFragment fragment = new MainContentFragment();
         fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).addToBackStack("mainContent").commit();
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -56,14 +57,19 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
         String functionTypeName = item.getTitle().toString();
+        Fragment currentlyVisible = fragmentManager.findFragmentById(R.id.fragment_container);
 
         switch (id) {
             case R.id.descriptive:
+                if (currentlyVisible instanceof DescriptiveInputFragment)
+                    break;
                 DescriptiveInputFragment descriptiveInputFragment = new DescriptiveInputFragment();
                 fragmentManager.beginTransaction()
                         .replace(R.id.fragment_container, descriptiveInputFragment).addToBackStack("input").commit();
                 break;
             case R.id.distributions:
+                if (currentlyVisible instanceof FunctionViewFragment)
+                    break;
                 FunctionViewFragment functionViewFragment = new FunctionViewFragment();
                 fragmentManager.beginTransaction()
                         .replace(R.id.fragment_container, functionViewFragment).addToBackStack("distribution").commit();
